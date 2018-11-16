fba95b3d8b75cf2ed2b5682a686a5416635787f5
FrLallemand <frlallemand@protonmail.com>
2018-11-16 10:50:55 +0100

    ajout des infos de backend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,13 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  backend: {
+    protocol: 'http',
+    host: '127.0.0.1',
+    port: '9000',
+    endpoints: {}
+  }
 };
 
 /*
