ab8ae89357469948a1a371daf698bf7a85d30eff
Simon HAJEK <simonhajek88@gmail.com>
2018-11-19 23:04:57 +0100

    Deleting spec

--- src/app/shared/pipes/is-send.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { IsSendPipe } from './is-send.pipe';
-
-describe('IsSendPipe', () => {
-  it('create an instance', () => {
-    const pipe = new IsSendPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
