0ba49929016a4e5dbceea6aad243d3e1f589f5e7
Simon HAJEK <simonhajek88@gmail.com>
2018-11-21 12:42:52 +0100

    Launch and wait for server

--- launch.sh
+++ launch.sh
@@ -1,3 +1,11 @@
+trap ctrl_c INT
+
+function ctrl_c() {
+  echo 'Killing node server ...'
+  kill $!
+  echo 'Killing angular cli ...'
+}
+
 git submodule init
 echo 'Pulling server ...'
 git submodule update --recursive --remote
@@ -5,14 +13,24 @@ echo 'Installing frontend ...'
 yarn install
 echo 'Launching frontend ...'
 ng serve &
-BACK_PID=$!
-wait $BACK_PID
 cd _server/backend
 echo 'Installing backend ...'
 yarn install
 echo 'Launching backend ...'
 npm run dev:watch &
-BACK_PID=$!
-wait $BACK_PID
-echo 'Ne pas oublier : Créer une base de données nommée : angulaaaaaargh et y executer le script init.mongo.js qui se trouve dans _server/backend/scripts'
-xdg-open http://localhost:4200/
+status_code=1
+while [[ "$status_code" -ne 200 ]];
+do
+  status_code=$(curl --write-out %{http_code} --silent --output /dev/null localhost:4443/documentation)
+
+  if [[ "$status_code" -eq 200 ]] ; then
+    echo 'Ne pas oublier : Créer une base de données nommée : angulaaaaaargh et y executer le script init.mongo.js qui se trouve dans _server/backend/scripts'
+    sleep 10
+    xdg-open http://localhost:4200/
+  fi
+done
+
+
+
+
+
